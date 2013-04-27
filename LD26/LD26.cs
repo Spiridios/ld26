50549a2653f2218d6137897197ef990eaa5d5058
haxmjl <1332337+Spiridios@users.noreply.github.com>
2013-04-26 23:08:22 -0700

    Get basic map up.

---
+++ LD26/LD26.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Spiridios.SpiridiEngine;
+
+namespace Spiridios.LD26
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class LD26 : SpiridiGame
+    {
+        public LD26()
+        {
+            this.LockFramerate = false;
+            this.SetWindowSize(640, 480);
+            this.NextState = new BootState(this, new TitleState(this));
+            this.ClearColor = new Color(0, 0, 0);
+            this.IsQuickExit = true;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            // Register key-bindings
+            this.InputManager.RegisterActionBinding("walkNorth", Keys.W);
+            this.InputManager.RegisterActionBinding("walkWest", Keys.A);
+            this.InputManager.RegisterActionBinding("walkSouth", Keys.S);
+            this.InputManager.RegisterActionBinding("walkEast", Keys.D);
+
+            this.ShowFPS = true;
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+            this.DefaultTextRenderer = new TextRenderer(this, "TitleScreenFont", new Color(0xAf, 0xaf, 0xaf));
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+    }
+}
---
+++ LD26/LD26States.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Spiridios.SpiridiEngine;
+using Spiridios.SpiridiEngine.Input;
+using Spiridios.SpiridiEngine.Scene;
+using System;
+
+namespace Spiridios.LD26
+{
+
+    public class TitleState : State
+    {
+        public TitleState(SpiridiGame game)
+            : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+            game.DrawText("Ludum Dare 26", TextRenderer.CENTERED, (game.WindowHeight - 10) / 2);
+            game.DrawText("Press Any Key To Start", TextRenderer.CENTERED, (game.WindowHeight + 30) / 2);
+            game.DrawFPS();
+        }
+
+        public override void KeyUp(KeyboardEvent keyState)
+        {
+            base.KeyUp(keyState);
+            game.NextState = new PlayGameState(game);
+            game.NextState.Initialize();
+        }
+
+        public override void KeyDown(KeyboardEvent keyState)
+        {
+            base.KeyDown(keyState);
+        }
+    }
+
+    public class PlayGameState : State
+    {
+        private Scene gameMap;
+
+        public PlayGameState(SpiridiGame game)
+            : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            gameMap = new Scene(game);
+            gameMap.LoadTiledMap("Map.tmx");
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+            gameMap.Draw(game.SpriteBatch);
+            game.DrawFPS();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            gameMap.Update(gameTime.ElapsedGameTime);
+        }
+
+    }
+
+
+}
