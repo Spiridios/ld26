68120a94fe8c4ecf1919985fc5177975b847188f
haxmjl <haxmjl@localhost>
2013-04-26 23:08:22 -0700

    Get basic map up.

--- LD26/Program.cs
+++ LD26/Program.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace LD26
+namespace Spiridios.LD26
 {
 #if WINDOWS || XBOX
     static class Program
@@ -10,7 +10,7 @@ namespace LD26
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (LD26 game = new LD26())
             {
                 game.Run();
             }
